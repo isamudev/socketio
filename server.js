2399325fdcf5ca6e790b55e37092d9400f8fbb63
Isamu <support@isamu.dev>
2024-01-09 22:04:05 -0300

    Update dependencies and add CORS configuration

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  }
+});
 
 io.on('connection', (socket) => {
   socket.on('emit', ($data) => {
-    io.emit($data.namespace, ($data.data || []))
+    if ($data.namespace && $data.namespace !== 'emit') {
+      io.emit($data.namespace, ($data.data || []))
+    }
   })
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
